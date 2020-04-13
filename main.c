aa43ac0ab25b25359d3a80aea94cc327b2645cb1
ylei1088@gmail.com <Rtf!xs9103>
2020-04-13 18:20:12 -0500

    add

--- main.c
+++ main.c
@@ -1,6 +1,37 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(){
+    srand(time(NULL));
+    int board[ 10 ][ 10 ]; // Two-dimensional array for gameboard.
 
+        // Function to initialize the gameboard.
+        
+        // create a blank board   
+        for (int x=0; x<10; x++) {
+            for (int y=0; y<10; y++) {
+                board[x][y] = 0;//0 = empty slot
+               
+            }  
+               
+        }     
+        
+        for(int x = 0;  x < 5; x++)
+        {
+     
+            board[rand() % 10][rand() % 10] = 1;
+            
+        }
+
+              std::cout << "\n";
+         for (int x=0; x<10; x++) {
+            for (int y=0; y<10; y++) {
+                std::cout << board[x][y] << " ";
+            }    
+            std::cout << "\n";
+        }  
 }
+
